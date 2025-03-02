2eaa0cabc2e47111086d3c647c35f530e35a2be9
Davi <daviprm@outlook.com>
2025-03-02 04:05:47 -0300

    feat: schema

---
+++ src/app.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from './auth/auth.guard';
+
+@Controller()
+export class AppController {
+  @Get()
+  getHello(): string {
+    return 'Hello World';
+  }
+}
---
+++ src/app.module.ts
@@ -0,0 +1,39 @@
+import { Global, MiddlewareConsumer, Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { CommandModule } from './command/command.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthController } from './auth/auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthGuard } from './auth/auth.guard';
+import { APP_GUARD } from '@nestjs/core';
+import { NotificationGateway } from './notification/notification.gateway';
+import { OrganizationModule } from './organization/organization.module';
+import { GroupModule } from './group/group.module';
+import { ModuleModule } from './module/module.module';
+import { PermissionModule } from './permission/permission.module';
+
+@Global()
+@Module({
+  imports: [
+    PrismaModule,
+    UserModule,
+    CommandModule,
+    AuthModule,
+    JwtModule,
+    OrganizationModule,
+    GroupModule,
+    ModuleModule,
+    PermissionModule,
+  ],
+  controllers: [AppController, AuthController],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    NotificationGateway,
+  ],
+})
+export class AppModule {}
